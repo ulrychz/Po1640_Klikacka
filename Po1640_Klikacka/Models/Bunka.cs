c14eaf66cf559d738bb77b19792c79bf3bf047f5
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-03-25 18:32:59 +0100

    Přidejte soubory projektu.

---
+++ Po1640_Klikacka/Models/Bunka.cs
@@ -0,0 +1,17 @@
+﻿namespace Po1640_Klikacka.Models
+{
+    public class Bunka
+    {
+        public Bunka(int radka, int sloupec, byte hodnota)
+        { 
+            Radka = radka;
+            Sloupec = sloupec;
+            Hodnota = hodnota;
+        }
+        public int Radka { get; private set; }
+        public int Sloupec { get; private set; }
+        public byte Hodnota { get; private set; }
+        public byte IndexBarvy { get; set; }
+        public string StylBarvy => $"barva-{IndexBarvy}";
+    }
+}
---
+++ Po1640_Klikacka/Models/Zaklad.cs
@@ -0,0 +1,36 @@
+﻿namespace Po1640_Klikacka.Models
+{
+    public class Zaklad
+    {
+        public Zaklad() 
+        {
+            NaplnNahodnePole(radku: 10, sloupcu: 12, cisloOd: 0, cisloDo: 1);
+        }
+        public Bunka[,] Matice { get; set; }
+        public byte ZvolenaBarva { get; set; } = 2;
+        private Random rnd = new Random();
+        private void NaplnNahodnePole(int radku, int sloupcu, byte cisloOd, byte cisloDo)
+        {
+            Matice = new Bunka[radku, sloupcu];
+            for (int i = 0; i < radku; i++)
+            {
+                for (int j = 0; j < sloupcu; j++)
+                {
+                    Matice[i, j] = new Bunka(i, j, (byte)rnd.Next(cisloOd, cisloDo + 1));
+                }
+            }
+        }
+
+        public void OnCellClick(Models.Bunka bunka)
+        {
+            if (bunka.Hodnota == 0 && !(bunka.IndexBarvy == ZvolenaBarva))
+            {
+                bunka.IndexBarvy = ZvolenaBarva;
+            }
+            else
+            {
+                bunka.IndexBarvy = 0;
+            }
+        }
+    }
+}
